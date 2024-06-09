8e345f71a0c5cbc3a6a5811ca00db40fb224e1b5
lexsalt <63517670+lexsalt@users.noreply.github.com>
2024-06-09 11:41:24 +0200

    before erasing player 2

--- public/index.js
+++ public/index.js
@@ -1,17 +1,17 @@
 // server.js
 let pitch1 = 0;
-let pitch2 = 0;
+// let pitch2 = 0;
 let roll1 = 0;
-let roll2 = 0;
+// let roll2 = 0;
 // document.getElementById("pitch").value = pitch;
 const canva = document.getElementById("canvas");
 const canva2 = document.getElementById("canvas2");
-const canva3 = document.getElementById("canvas3");
-const canva4 = document.getElementById("canvas4");
+// const canva3 = document.getElementById("canvas3");
+// const canva4 = document.getElementById("canvas4");
 const ctx = canva.getContext("2d");
 const ctx2 = canva2.getContext("2d");
-const ctx3 = canva3.getContext("2d");
-const ctx4 = canva4.getContext("2d");
+// const ctx3 = canva3.getContext("2d");
+// const ctx4 = canva4.getContext("2d");
 let divClientWidth = document.getElementById("container").clientWidth;
 console.log(divClientWidth);
 let divClientHeight = document.getElementById("container").clientHeight;
@@ -19,19 +19,18 @@ console.log(divClientHeight);
 let width = divClientWidth;
 let height = divClientHeight;
 
-
-canvas.height = height
+canvas.height = height;
 canvas.width = width;
-canva2.height = height
+canva2.height = height;
 canva2.width = width;
-canva3.height = height
-canva3.width = width;
-canva4.height = height
-canva4.width = width;
-let pitchArray = [];
-let rollArray = [];
-// let yaw = 0;
-// let yawArray = [];
+// canva3.height = height
+// canva3.width = width;
+// canva4.height = height
+// canva4.width = width;
+let pitch1Array = [];
+let roll1Array = [];
+let yaw1 = 0;
+let yaw1Array = [];
 // let accelX = 0;
 // let accelXArray = [];
 // let accelY = 0;
@@ -39,18 +38,18 @@ let rollArray = [];
 // let accelZ = 0;
 // let accelZArray = [];
 let a1 = false;
-let a2 = false;
+// let a2 = false;
 // let b = false;
 // let ab = false;
 const sampleSize = 10;
 const centerX = width / 2;
 const start1X = centerX / 2;
-const start2X = centerX+(centerX / 2);
+// const start2X = centerX+(centerX / 2);
 const centerY = height / 2;
 let positionX1 = start1X;
 let positionY1 = centerY;
-let positionX2 = start2X;
-let positionY2 = centerY;
+// let positionX2 = start2X;
+// let positionY2 = centerY;
 console.log("Hello World!");
 const socket = io();
 
@@ -72,8 +71,8 @@ function processData(data) {
         if (!checkNum(num)) {
           // console.log(num);
         } else if (checkNum(num)) {
-          arrayPitchPushValues(num, pitchArray, sampleSize);
-          pitch1 = mostFrequent(pitchArray, pitchArray.length);
+          arrayPitchPushValues(num, pitch1Array, sampleSize);
+          pitch1 = mostFrequent(pitch1Array, pitch1Array.length);
           console.log(`pitch: ${pitch1}`);
         }
       } else {
@@ -93,20 +92,39 @@ function processData(data) {
         if (!checkNum(num)) {
           // console.log(num);
         } else if (checkNum(num)) {
-          arrayRollPushValues(num, rollArray, sampleSize);
-          roll1 = mostFrequent(rollArray, rollArray.length);
+          arrayRollPushValues(num, roll1Array, sampleSize);
+          roll1 = mostFrequent(roll1Array, roll1Array.length);
+          console.log(`roll: ${roll1}`);
+        }
+      } else {
+        console.log("Not a valid Roll");
+      }
+    } else if (data.indexOf("y") === 0) {
+      let arr = data.split("");
+      if (arr[0] === "y") {
+        arr.shift();
+        let str = arr.join("");
+        let num = Number(str);
+        // console.log(num)
+        // console.log(str)
+        // console.log(arr)
+        if (!checkNum(num)) {
+          // console.log(num);
+        } else if (checkNum(num)) {
+          arrayYawPushValues(num, yaw1Array, sampleSize);
+          roll1 = mostFrequent(yaw1Array, yaw1Array.length);
           console.log(`roll: ${roll1}`);
         }
       } else {
         console.log("Not a valid Roll");
       }
     } else if (data.indexOf("a") === 0) {
-        a1 = true;
-        setTimeout(() => {
-          // console.log("Delayed for 0.5 second.");
-          a1 = false;
-        }, "500");
-      }
+      a1 = true;
+      setTimeout(() => {
+        // console.log("Delayed for 0.5 second.");
+        a1 = false;
+      }, "500");
+    }
   }
 }
 function processData2(data) {
@@ -127,7 +145,7 @@ function processData2(data) {
         if (!checkNum(num)) {
           // console.log(num);
         } else if (checkNum(num)) {
-          pitch1= num;
+          pitch1 = num;
           console.log(`pitch: ${pitch1}`);
         }
       } else {
@@ -174,7 +192,7 @@ function processData3(data) {
         if (!checkNum(num)) {
           // console.log(num);
         } else if (checkNum(num)) {
-          pitch1= num;
+          pitch1 = num;
           console.log(`pitch: ${pitch1}`);
         }
       } else {
@@ -213,7 +231,7 @@ function processData3(data) {
         if (!checkNum(num)) {
           // console.log(num);
         } else if (checkNum(num)) {
-          pitch2= num;
+          pitch2 = num;
           console.log(`pitch: ${pitch2}`);
         }
       } else {
@@ -266,7 +284,7 @@ function arrayPitchPushValues(num, arr, sample) {
     arr.unshift(num);
   }
   // console.log(arr)
-  pitchArray = arr;
+  pitch1Array = arr;
 }
 function arrayRollPushValues(num, arr, sample) {
   if (arr.length > sample) {
@@ -276,7 +294,7 @@ function arrayRollPushValues(num, arr, sample) {
     arr.unshift(num);
   }
   // console.log(arr)
-  rollArray = arr;
+  roll1Array = arr;
 }
 function arrayYawPushValues(num, arr, sample) {
   if (arr.length > sample) {
@@ -286,7 +304,7 @@ function arrayYawPushValues(num, arr, sample) {
     arr.unshift(num);
   }
   // console.log(arr)
-  yawArray = arr;
+  yaw1Array = arr;
 }
 function mostFrequent(arr, n) {
   let maxcount = 0;
@@ -323,97 +341,97 @@ function getRandomColor() {
 var painting = a1; //draw variable
 
 function startPosition() {
-    painting = true;
-    draw1();
+  painting = true;
+  draw1();
 }
 function finishedPosition() {
-    painting = false;
-    ctx.beginPath();
+  painting = false;
+  ctx.beginPath();
 }
 
 function draw1() {
-    // if (!painting) return;
-    document.getElementById("pitch1").value = pitch1;
-    document.getElementById("roll1").value = roll1;
-    document.getElementById("positionX1").value = positionX1.toFixed(2);
-    document.getElementById("positionY1").value = positionY1.toFixed(2);
-    positionX1 = setPositionX1(positionX1);
-    positionY1 = setPositionY1(positionY1);
-    ctx.strokeStyle = "black";
-    ctx.fillStyle = "black";
-    ctx.lineWidth = 6;
-    ctx.lineCap = 'round';
-    ctx.lineTo(positionX1, positionY1);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(positionX1, positionY1);
+  // if (!painting) return;
+  document.getElementById("pitch1").value = pitch1;
+  document.getElementById("roll1").value = roll1;
+  document.getElementById("positionX1").value = positionX1.toFixed(2);
+  document.getElementById("positionY1").value = positionY1.toFixed(2);
+  positionX1 = setPositionX1(positionX1);
+  positionY1 = setPositionY1(positionY1);
+  ctx.strokeStyle = "black";
+  ctx.fillStyle = "black";
+  ctx.lineWidth = 6;
+  ctx.lineCap = "round";
+  ctx.lineTo(positionX1, positionY1);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(positionX1, positionY1);
 }
-function draw2() {
-    // if (!painting) return;
-    document.getElementById("pitch2").value = pitch2;
-    document.getElementById("roll2").value = roll2;
-    document.getElementById("positionX2").value = positionX2.toFixed(2);
-    document.getElementById("positionY2").value = positionY2.toFixed(2);
-    positionX2 = setPositionX2(positionX2);
-    positionY2 = setPositionY2(positionY2);
-    ctx3.strokeStyle = "black";
-    ctx3.fillStyle = "black";
-    ctx3.lineWidth = 6;
-    ctx3.lineCap = 'round';
-    ctx3.lineTo(positionX2, positionY2);
-    ctx3.stroke();
-    ctx3.beginPath();
-    ctx3.moveTo(positionX2, positionY2);
-}
+// function draw2() {
+//     // if (!painting) return;
+//     document.getElementById("pitch2").value = pitch2;
+//     document.getElementById("roll2").value = roll2;
+//     document.getElementById("positionX2").value = positionX2.toFixed(2);
+//     document.getElementById("positionY2").value = positionY2.toFixed(2);
+//     positionX2 = setPositionX2(positionX2);
+//     positionY2 = setPositionY2(positionY2);
+//     ctx3.strokeStyle = "black";
+//     ctx3.fillStyle = "black";
+//     ctx3.lineWidth = 6;
+//     ctx3.lineCap = 'round';
+//     ctx3.lineTo(positionX2, positionY2);
+//     ctx3.stroke();
+//     ctx3.beginPath();
+//     ctx3.moveTo(positionX2, positionY2);
+// }
 
 function pointer1() {
-    ctx2.clearRect(0, 0, width, height);
-    ctx2.beginPath();
-    ctx2.lineColor = "red";
-    ctx2.strokeStyle = "red";
-    ctx2.moveTo(positionX1+5, positionY1-10);
-    ctx2.lineTo(positionX1+20,positionY1-15);
-    ctx2.lineTo(positionX1+5, positionY1-25);
-    ctx2.lineTo(positionX1+5, positionY1-10);
-    ctx2.fillStyle = "red";
-    ctx2.fill();
-    ctx2.stroke();
+  ctx2.clearRect(0, 0, width, height);
+  ctx2.beginPath();
+  ctx2.lineColor = "red";
+  ctx2.strokeStyle = "red";
+  ctx2.moveTo(positionX1 + 5, positionY1 - 10);
+  ctx2.lineTo(positionX1 + 20, positionY1 - 15);
+  ctx2.lineTo(positionX1 + 5, positionY1 - 25);
+  ctx2.lineTo(positionX1 + 5, positionY1 - 10);
+  ctx2.fillStyle = "red";
+  ctx2.fill();
+  ctx2.stroke();
 }
-function pointer2() {
-    // if (!painting) return;
-    ctx4.clearRect(0, 0, width, height);
-    ctx4.beginPath();
-    ctx4.lineColor = "gray";
-    ctx4.moveTo(positionX2+5, positionY2-10);
-    ctx4.lineTo(positionX2+20, positionY2-15);
-    ctx4.lineTo(positionX2+5, positionY2-25);
-    ctx4.lineTo(positionX2+5, positionY2-10);
-    ctx4.fillStyle = "red";
-    ctx4.fill();
-    ctx4.stroke();
+// function pointer2() {
+//     // if (!painting) return;
+//     ctx4.clearRect(0, 0, width, height);
+//     ctx4.beginPath();
+//     ctx4.lineColor = "gray";
+//     ctx4.moveTo(positionX2+5, positionY2-10);
+//     ctx4.lineTo(positionX2+20, positionY2-15);
+//     ctx4.lineTo(positionX2+5, positionY2-25);
+//     ctx4.lineTo(positionX2+5, positionY2-10);
+//     ctx4.fillStyle = "red";
+//     ctx4.fill();
+//     ctx4.stroke();
+// }
+function text1() {
+  ctx2.clearRect(0, 0, width, height);
+  ctx2.lineWidth = 1;
+  ctx2.strokeStyle = "white";
+  ctx2.font = "bolder 12px Verdana";
+  ctx2.fillStyle = "black";
+  ctx2.strokeText("Player 1", positionX1 + 15, positionY1 - 25);
+  ctx2.fillText("Player 1", positionX1 + 15, positionY1 - 25);
+  ctx2.fill();
+  ctx2.stroke();
 }
-function text1 () {
-    ctx2.clearRect(0, 0, width, height);
-    ctx2.lineWidth = 1;
-    ctx2.strokeStyle = "white";
-    ctx2.font = "bolder 12px Verdana";
-    ctx2.fillStyle = "black";
-    ctx2.strokeText("Player 1", positionX1+15, positionY1-25);
-    ctx2.fillText("Player 1", positionX1+15, positionY1-25);
-    ctx2.fill();
-    ctx2.stroke();
-} 
-function text2 () {
-    ctx4.clearRect(0, 0, width, height);
-    ctx4.lineWidth = 1;
-    ctx4.strokeStyle = "white";
-    ctx4.font = "bolder 12px Verdana";
-    ctx4.fillStyle = "black";
-    ctx4.strokeText("Player 2", positionX2+15, positionY2-25);
-    ctx4.fillText("Player 2", positionX2+15, positionY2-25);
-    ctx4.fill();
-    ctx4.stroke();
-} 
+// function text2 () {
+//     ctx4.clearRect(0, 0, width, height);
+//     ctx4.lineWidth = 1;
+//     ctx4.strokeStyle = "white";
+//     ctx4.font = "bolder 12px Verdana";
+//     ctx4.fillStyle = "black";
+//     ctx4.strokeText("Player 2", positionX2+15, positionY2-25);
+//     ctx4.fillText("Player 2", positionX2+15, positionY2-25);
+//     ctx4.fill();
+//     ctx4.stroke();
+// }
 
 function setPositionX1(x) {
   if (x > width) {
@@ -421,54 +439,52 @@ function setPositionX1(x) {
   } else if (x < 10) {
     x = 10;
   } else {
-    x = x + (roll1 / 100);
+    x = x + roll1 / 100;
   }
   return x;
 }
-function setPositionX2(x) {
-  if (x > width) {
-    x = width - 10;
-  } else if (x < 10) {
-    x = 10;
-  } else {
-    x = x + (roll2 / 100);
-  }
-  return x;
-}
+// function setPositionX2(x) {
+//   if (x > width) {
+//     x = width - 10;
+//   } else if (x < 10) {
+//     x = 10;
+//   } else {
+//     x = x + (roll2 / 100);
+//   }
+//   return x;
+// }
 function setPositionY1(y) {
   if (y > height) {
     y = height - 10;
   } else if (y < 10) {
     y = 10;
   } else {
-    y = y + (pitch1/ 100);
+    y = y + pitch1 / 100;
   }
   return y;
 }
-function setPositionY2(y) {
-  if (y > height) {
-    y = height - 10;
-  } else if (y < 10) {
-    y = 10;
-  } else {
-    y = y + (pitch2/ 100);
-  }
-  return y;
-}
+// function setPositionY2(y) {
+//   if (y > height) {
+//     y = height - 10;
+//   } else if (y < 10) {
+//     y = 10;
+//   } else {
+//     y = y + (pitch2/ 100);
+//   }
+//   return y;
+// }
 
 function execute() {
   pointer1();
   draw1();
   text1();
-  pointer2();
-  draw2();
-  text2();
+  // pointer2();
+  // draw2();
+  // text2();
 }
-setInterval(
-  execute,
-  10);
+setInterval(execute, 10);
 
-window.addEventListener('resize', function () {
-    canvas.height = window.innerHeight;
-    canvas.width = divClientWidth;
-})
+window.addEventListener("resize", function () {
+  canvas.height = window.innerHeight;
+  canvas.width = divClientWidth;
+});
